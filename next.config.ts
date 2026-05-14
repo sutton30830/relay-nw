30ecfa55a911fa7802d78312bb2f868b006676df
sutton30830 <147671103+sutton30830@users.noreply.github.com>
2026-05-13 17:54:22 -0700

    Add optional Sentry instrumentation

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
+import type { Configuration } from "webpack";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack(config: Configuration) {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings ?? []),
+      {
+        module: /@opentelemetry\/instrumentation/,
+        message: /Critical dependency: the request of a dependency is an expression/,
+      },
+    ];
+
+    return config;
+  },
+};
 
 export default nextConfig;
